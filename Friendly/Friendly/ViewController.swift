474813b1fe0a54180b349bb3a59edd73d523b28e
Hanc <naum.hanc@kesolutions.com>
2021-03-17 12:20:34 +0200

    V1
    First Commit

---
+++ Friendly/Friendly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Friendly
+//
+//  Created by Hanc on 3/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
