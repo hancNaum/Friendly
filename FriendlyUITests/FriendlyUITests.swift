042be8892955a0e4efa81e9bd8b3ddbb84774eef
Hanc <naum.hanc@kesolutions.com>
2021-03-25 11:56:56 +0200

    faild test

--- FriendlyUITests/FriendlyUITests.swift
+++ FriendlyUITests/FriendlyUITests.swift
@@ -30,7 +30,7 @@ class FriendlyUITests: XCTestCase {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         let i = 0
-        XCTAssert(i==0, "i allways should be 0")
+        XCTAssert(i==1, "i allways should be 0")
     }
 
     func testLaunchPerformance() throws {
